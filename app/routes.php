0e092bd63df7261c3613ab52689cc02ad9b8464e
Joe Tannenbaum <josephtannenbaum@gmail.com>
2014-07-05 21:47:31 -0400

    added roku screensaver route, started using memcached

--- app/routes.php
+++ app/routes.php
@@ -15,3 +15,58 @@ Route::get('/', function()
 {
 	return View::make('hello');
 });
+
+
+Route::get('/roku-screensaver', function()
+{
+	$cache_key = 'roku-screensaver';
+
+	$return_photos = \Cache::get( $cache_key );
+
+	if ( !$return_photos )
+	{
+		$client = new \GuzzleHttp\Client(['base_url' => 'https://api.dropbox.com/1/']);
+
+		$response = $client->get('metadata/auto/Photos%2FRoku%20Screensaver', [
+				'headers' => [
+					'Authorization' => 'Bearer ' . getenv('dropbox.access_token'),
+				],
+				'query' => [
+					'locale'     => '',
+					'list'       => TRUE,
+					'file_limit' => 25000,
+				]
+			]);
+
+		$files = $response->json();
+
+		$photos = $files['contents'];
+
+		$return_photos = [];
+
+		foreach ( $photos as $p )
+		{
+			$response = $client->get('media/dropbox' . $p['path'], [
+					'headers' => [
+						'Authorization' => 'Bearer ' . getenv('dropbox.access_token'),
+					],
+				]);
+
+			$photo = $response->json();
+
+			$photo_info = getimagesize( $photo['url'] );
+
+			$return_photos[] = [
+					'url'    => $photo['url'],
+					'width'  => $photo_info[ 0 ],
+					'height' => $photo_info[ 1 ],
+				];
+		}
+
+		\Cache::put( $cache_key, $return_photos, \Carbon\Carbon::now()->addHours(4) );
+	}
+
+	shuffle( $return_photos );
+
+	return $return_photos;
+});
