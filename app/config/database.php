97dfe31b98a66ece91de3b057db8c0c9a2c8e07e
Joe Tannenbaum <josephtannenbaum@gmail.com>
2014-07-15 21:48:08 -0400

    new database config

--- app/config/database.php
+++ app/config/database.php
@@ -26,7 +26,7 @@ return array(
 	|
 	*/
 
-	'default' => 'sqlite',
+	'default' => 'mysql',
 
 	/*
 	|--------------------------------------------------------------------------
@@ -55,9 +55,9 @@ return array(
 		'mysql' => array(
 			'driver'    => 'mysql',
 			'host'      => 'localhost',
-			'database'  => 'database',
-			'username'  => 'root',
-			'password'  => '',
+			'database'  => getenv('mysql.database'),
+			'username'  => getenv('mysql.username'),
+			'password'  => getenv('mysql.password'),
 			'charset'   => 'utf8',
 			'collation' => 'utf8_unicode_ci',
 			'prefix'    => '',
